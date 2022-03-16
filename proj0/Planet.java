0f07c00f52eb93438908bc72615b1f9d0a61c89b
Wings George <wings@WingsdeMacBook-Pro.local>
2022-03-16 14:18:31 +0800

    complete proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -16,7 +16,7 @@ public class Planet
 
 	public String imgFileName;
 
-	public static final double G = 6.67e-11;
+	
 
 	public Planet(double xP, double yP, double xV,
 				double yV, double m, String img)
@@ -49,6 +49,7 @@ public class Planet
 
 	public double calcForceExertedBy(Planet b)
 	{
+		double G = 6.67e-11;
 		double r1 = calcDistance(b);
 		double F = G * this.mass * b.mass / (r1*r1);
 		return F;
